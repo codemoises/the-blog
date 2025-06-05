0f03a3c7c7bb81249e4b4b7686d807981d77e999
codemoises <88902469+codemoises@users.noreply.github.com>
2025-06-05 16:49:24 -0300

    correção na crase

--- src/utils/format-datetime.ts
+++ src/utils/format-datetime.ts
@@ -3,7 +3,7 @@ import { ptBR } from 'date-fns/locale';
 
 export function formatDatetime(rawDate: string): string {
   const date = new Date(rawDate);
-  return format(date, "dd/MM/yyyy 'ás' HH'h'mm", {
+  return format(date, "dd/MM/yyyy 'às' HH'h'mm", {
     locale: ptBR,
   });
 }
