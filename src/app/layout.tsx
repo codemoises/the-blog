748bc8c0d33e2a855784abe4dfac19b0caf58150
codemoises <88902469+codemoises@users.noreply.github.com>
2025-06-10 17:11:51 -0300

    componente Footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import './globals.css';
 import { Container } from '@/components/Container';
 import { Header } from '@/components/Header';
+import { Footer } from '@/components/Footer';
 
 export const metadata: Metadata = {
   title: {
@@ -21,11 +22,10 @@ export default function RootLayout({
       <body>
         <Container>
           <Header />
+
           {children}
 
-          <footer>
-            <p className='text-6xl font-bold text-center py-8'>FOOTER</p>
-          </footer>
+          <Footer />
         </Container>
       </body>
     </html>
--- src/components/PostsList/index.tsx
+++ src/components/PostsList/index.tsx
@@ -6,7 +6,7 @@ export async function PostsList() {
   const posts = await findAllPublicPosts();
 
   return (
-    <div className='grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3'>
+    <div className='grid grid-cols-1 mb-16 gap-8 sm:grid-cols-2 lg:grid-cols-3'>
       {posts.map(post => {
         const postLink = `/post/${post.slug}`;
         return (
