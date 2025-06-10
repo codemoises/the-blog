ece5015a393d2ef3b36fff6930cc3ab2c68cdf65
codemoises <88902469+codemoises@users.noreply.github.com>
2025-06-09 22:35:08 -0300

    React cache remove duplicação de chamadas da API ou Repositório

--- src/components/PostFeatured/index.tsx
+++ src/components/PostFeatured/index.tsx
@@ -1,9 +1,11 @@
+import { findAllPublicPosts } from '@/lib/post/queries';
 import { PostCoverImage } from '../PostCoverImage';
 import { PostSummary } from '../PostSummary';
 
-export function PostFeatured() {
-  const slug = 'qualquer';
-  const postLink = `/post/${slug}`;
+export async function PostFeatured() {
+  const posts = await findAllPublicPosts();
+  const post = posts[0];
+  const postLink = `/post/${post.slug}`;
   return (
     <section className='grid grid-cols-1 gap-8 mb-16 sm:grid-cols-2 group'>
       <PostCoverImage
