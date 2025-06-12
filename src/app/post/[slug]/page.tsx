94071c4b9b14b1078211c91184d51aba1cdfc5c4
codemoises <88902469+codemoises@users.noreply.github.com>
2025-06-12 15:02:55 -0300

    Melhoria na função findPostBySlugCached para reuso

--- src/app/post/[slug]/page.tsx
+++ src/app/post/[slug]/page.tsx
@@ -1,5 +1,4 @@
 import { findPostBySlugCached } from '@/lib/post/queries';
-import { notFound } from 'next/navigation';
 
 type PostSlugPageProps = {
   params: Promise<{ slug: string }>;
@@ -7,15 +6,8 @@ type PostSlugPageProps = {
 export default async function PostSlugPage({ params }: PostSlugPageProps) {
   const { slug } = await params;
 
-  let post;
+  const post = await findPostBySlugCached(slug);
 
-  try {
-    post = await findPostBySlugCached(slug);
-  } catch {
-    post = undefined;
-  }
-
-  if (!post) notFound();
   return (
     <div>
       <p>{post.title}</p>
