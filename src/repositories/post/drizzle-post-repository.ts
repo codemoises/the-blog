2522bd29b07d82e9fdf8286b59b9efba5436c207
codemoises <88902469+codemoises@users.noreply.github.com>
2025-06-17 17:28:36 -0300

    cria o repositório DrizzlePostRepository

---
+++ src/repositories/post/drizzle-post-repository.ts
@@ -0,0 +1,25 @@
+import { PostModel } from '@/models/post/post-model';
+import { PostRepository } from './post-repository';
+import { drizzleDb } from '@/db/drizzle';
+import { postsTable } from '@/db/drizzle/schemas';
+import { desc, eq } from 'drizzle-orm';
+
+export class DrizzlePostRepository implements PostRepository {
+  async findAllPublic(): Promise<PostModel[]> {
+    const posts = await drizzleDb.query.posts.findMany({
+      orderBy: (posts, { desc }) => desc(posts.createdAt),
+      where: (posts, { eq }) => eq(posts.published, true),
+    });
+  }
+
+  async findBySlugPublic(slug: string): Promise<PostModel> {}
+
+  async findAll(): Promise<PostModel[]> {}
+
+  async findById(id: string): Promise<PostModel> {}
+}
+
+(async () => {
+  const repo = new DrizzlePostRepository();
+  await repo.findAllPublic();
+})();
