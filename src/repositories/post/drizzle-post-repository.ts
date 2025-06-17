a3b7127e87eae2dd80376202ecc68151d55c2e6c
codemoises <88902469+codemoises@users.noreply.github.com>
2025-06-17 18:05:11 -0300

    continuação da criação do repositório DrizzlePostRepository

--- src/repositories/post/drizzle-post-repository.ts
+++ src/repositories/post/drizzle-post-repository.ts
@@ -1,8 +1,6 @@
 import { PostModel } from '@/models/post/post-model';
 import { PostRepository } from './post-repository';
 import { drizzleDb } from '@/db/drizzle';
-import { postsTable } from '@/db/drizzle/schemas';
-import { desc, eq } from 'drizzle-orm';
 
 export class DrizzlePostRepository implements PostRepository {
   async findAllPublic(): Promise<PostModel[]> {
@@ -10,16 +8,49 @@ export class DrizzlePostRepository implements PostRepository {
       orderBy: (posts, { desc }) => desc(posts.createdAt),
       where: (posts, { eq }) => eq(posts.published, true),
     });
+
+    return posts;
   }
 
-  async findBySlugPublic(slug: string): Promise<PostModel> {}
+  async findBySlugPublic(slug: string): Promise<PostModel> {
+    const post = await drizzleDb.query.posts.findFirst({
+      where: (posts, { eq, and }) =>
+        and(eq(posts.published, true), eq(posts.slug, slug)),
+    });
 
-  async findAll(): Promise<PostModel[]> {}
+    if (!post) throw new Error('Post não encontrado para slug');
 
-  async findById(id: string): Promise<PostModel> {}
+    return post;
+  }
+
+  async findAll(): Promise<PostModel[]> {
+    const posts = await drizzleDb.query.posts.findMany({
+      orderBy: (posts, { desc }) => desc(posts.createdAt),
+    });
+
+    return posts;
+  }
+
+  async findById(id: string): Promise<PostModel> {
+    const post = await drizzleDb.query.posts.findFirst({
+      where: (posts, { eq }) => eq(posts.id, id),
+    });
+
+    if (!post) throw new Error('Post não encontrado para ID');
+
+    return post;
+  }
 }
 
-(async () => {
-  const repo = new DrizzlePostRepository();
-  await repo.findAllPublic();
-})();
+// (async () => {
+//   // como-a-tecnologia-impacta-nosso-bem-estar false
+//   // os-desafios-do-trabalho-remoto-moderno true
+//   const repo = new DrizzlePostRepository();
+//   const posts = await repo.findAll();
+
+//   // posts.forEach(post => console.log(post.id, post.published));
+//   const post = await repo.findBySlugPublic(
+//     'os-desafios-do-trabalho-remoto-moderno ',
+//   );
+//   console.log(post);
+// })();
