8631ab9e2faac7326ac0758d49407c7ad50a24a8
codemoises <88902469+codemoises@users.noreply.github.com>
2025-06-24 14:44:04 -0300

    move o botão para DELETE do post para um Client Component (use client)

--- src/components/PostListAdmin/index.tsx
+++ src/components/PostListAdmin/index.tsx
@@ -1,7 +1,8 @@
+import { deletePostAction } from '@/actions/post/delete-post-action';
 import { findAllPostAdmin } from '@/lib/post/queries/admin';
 import clsx from 'clsx';
-import { Trash2Icon } from 'lucide-react';
 import Link from 'next/link';
+import { DeletePostButton } from '../Admin/DeletePostButton';
 
 export default async function PostsListAdmin() {
   const posts = await findAllPostAdmin();
@@ -25,17 +26,10 @@ export default async function PostsListAdmin() {
               </span>
             )}
 
-            <button
-              className={clsx(
-                'text-red-500 cursor-pointer',
-                '[&_svg]:w-4 [&_svg]:h-4',
-                'hover:scale-120 hover:text-red-700',
-              )}
-              aria-label={`Apagar post: ${post.title}`}
-              title={`Apagar post: ${post.title}`}
-            >
-              <Trash2Icon />
-            </button>
+            <form action={deletePostAction}>
+              <input type='hidden' name='id' defaultValue={post.id} />
+              <DeletePostButton id={post.id} title={post.title} />
+            </form>
           </div>
         );
       })}
