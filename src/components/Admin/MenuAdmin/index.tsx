a82652df8058a8497b3a842e106254cfd631e979
codemoises <88902469+codemoises@users.noreply.github.com>
2025-06-26 15:38:45 -0300

    estilização do MenuAdmin

--- src/components/Admin/MenuAdmin/index.tsx
+++ src/components/Admin/MenuAdmin/index.tsx
@@ -1,13 +1,45 @@
+import clsx from 'clsx';
+import { FileTextIcon, HouseIcon } from 'lucide-react';
 import Link from 'next/link';
 
 export function MenuAdmin() {
+  const navClasses = clsx(
+    // 'h-10',
+    // 'overflow-hidden',
+    'flex flex-col mb-8',
+    'bg-slate-900 text-slate-100 rounded-lg',
+    'sm:flex-row sm:flex-wrap',
+  );
+  const linkClasses = clsx(
+    'flex items-center justify-start gap-2',
+    '[&>svg]:w-[16px] [&>svg]:h-[16px] px-4 rounded-lg',
+    'transition hover:bg-slate-800',
+    'h-10',
+    'shrink-0',
+  );
   return (
-    <nav>
-      <a href='/' target='_blank'>
+    <nav className={navClasses}>
+      <a className={linkClasses} href='/' target='_blank'>
+        <HouseIcon />
         Home
       </a>
 
-      <Link href={'/admin/post'}>Posts</Link>
+      <Link className={linkClasses} href={'/admin/post'}>
+        <FileTextIcon />
+        Posts
+      </Link>
+      <Link className={linkClasses} href={'/admin/post'}>
+        <FileTextIcon />
+        Posts
+      </Link>
+      <Link className={linkClasses} href={'/admin/post'}>
+        <FileTextIcon />
+        Posts
+      </Link>
+      <Link className={linkClasses} href={'/admin/post'}>
+        <FileTextIcon />
+        Posts
+      </Link>
     </nav>
   );
 }
