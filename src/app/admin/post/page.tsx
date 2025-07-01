e77577bf999a9a9e58fa9c56446d37511ae33837
codemoises <88902469+codemoises@users.noreply.github.com>
2025-07-01 13:18:10 -0300

    Componente InputCheckBox

--- src/app/admin/post/page.tsx
+++ src/app/admin/post/page.tsx
@@ -1,28 +1,36 @@
+import { Button } from '@/components/Button';
+import { InputCheckBox } from '@/components/InputCheckBox';
 import { InputText } from '@/components/InputText';
 
 export const dynamic = 'force-dynamic';
 
 export default async function AdminPostPage() {
   return (
-    <div className='flex flex-col gap-6'>
-      <InputText labelText='Nome' placeholder='Digite seu nome' />
-      <InputText labelText='Sobrenome' placeholder='Digite seu sobrenome' />
-      <InputText
-        disabled
-        labelText='Sobrenome'
-        placeholder='Digite seu sobrenome'
-      />
-      <InputText
-        disabled
-        labelText='Sobrenome'
-        placeholder='Digite seu sobrenome'
-      />
-      <InputText
-        labelText='Sobrenome'
-        placeholder='Digite seu sobrenome'
-        readOnly
-      />
-      <InputText labelText='Sobrenome' placeholder='Digite seu sobrenome' />
-    </div>
+    <form action='' className='mb-16'>
+      <div className='flex flex-col gap-6'>
+        <InputText labelText='Nome' placeholder='Digite seu nome' />
+        <InputText labelText='Sobrenome' placeholder='Digite seu sobrenome' />
+        <InputCheckBox labelText='Sobrenome' />
+        <InputText
+          disabled
+          labelText='Sobrenome'
+          placeholder='Digite seu sobrenome'
+        />
+        <InputText
+          disabled
+          labelText='Sobrenome'
+          placeholder='Digite seu sobrenome'
+        />
+        <InputText
+          labelText='Sobrenome'
+          placeholder='Digite seu sobrenome'
+          readOnly
+        />
+        <InputText labelText='Sobrenome' placeholder='Digite seu sobrenome' />
+        <div className='mt-4'>
+          <Button type='submit'>Enviar</Button>
+        </div>
+      </div>
+    </form>
   );
 }
