c58eba681c4163ac4a2b17d56bfb0dad712e64fd
codemoises <88902469+codemoises@users.noreply.github.com>
2025-07-02 23:22:58 -0300

    valida o tamanho do arquivo no front-end

--- src/components/Admin/ImageUploader/index.tsx
+++ src/components/Admin/ImageUploader/index.tsx
@@ -1,8 +1,10 @@
 'use client';
 
 import { Button } from '@/components/Button';
+import { IMAGE_UPLOAD_MAX_SIZE } from '@/lib/constants';
 import { ImageUpIcon } from 'lucide-react';
 import { useRef } from 'react';
+import { toast } from 'react-toastify';
 
 export function ImageUploader() {
   const fileInputRef = useRef<HTMLInputElement>(null);
@@ -10,6 +12,31 @@ export function ImageUploader() {
     if (!fileInputRef.current) return;
 
     fileInputRef.current.click();
+  }
+
+  function handleChange() {
+    if (!fileInputRef.current) return;
+
+    const fileInput = fileInputRef.current;
+    const file = fileInput?.files?.[0];
+
+    if (!file) return;
+
+    if (file.size > IMAGE_UPLOAD_MAX_SIZE) {
+      const readableMaxSize = IMAGE_UPLOAD_MAX_SIZE / 1024;
+      toast.error(`Imagem muito grande. Max.: ${readableMaxSize}KB.`);
+
+      fileInput.value = '';
+
+      return;
+    }
+
+    const formData = new FormData();
+    formData.append('file', file);
+
+    console.log(formData.get('file'));
+
+    fileInput.value = '';
   }
   return (
     <div className='flex flex-col gap-2 py-4'>
@@ -19,6 +46,7 @@ export function ImageUploader() {
       </Button>
 
       <input
+        onChange={handleChange}
         ref={fileInputRef}
         className='hidden'
         name='file'
