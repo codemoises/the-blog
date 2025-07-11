34161321ea2aa4830c5f549423f292c59555d5f4
codemoises <88902469+codemoises@users.noreply.github.com>
2025-07-11 16:11:34 -0300

    se o usuário e senha são válidos, vamos gerar o cookie e o JWT

--- src/actions/login/login-action.ts
+++ src/actions/login/login-action.ts
@@ -22,9 +22,28 @@ export async function loginAction(state: LoginActionState, formData: FormData) {
   const username = formData.get('username')?.toString().trim() || '';
   const password = formData.get('password')?.toString().trim() || '';
 
+  if (!username || !password) {
+    return {
+      username,
+      error: 'Digite o usuário e a senha',
+    };
+  }
+
+  const isUsernameValid = username === process.env.LOGIN_USER;
+  const isPasswordValid = await verifyPassword(
+    password,
+    process.env.LOGIN_PASS || '',
+  );
+
+  if (!isUsernameValid || !isPasswordValid) {
+    return {
+      username,
+      error: 'Usuário ou senha inválidos',
+    };
+  }
 
   return {
-    username: '',
-    error: '',
+    username,
+    error: 'Usuário logado com sucesso',
   };
 }
